ca3e972d58fecd585c4b7f9f173c5f6325a53463
dodo_lihao李灏 <2954722256@qq.com>
2018-06-03 14:58:55 +0800

    heroes list
    heroes list的 component

--- toh-pt07restart/src/app/heroes/heroes.component.ts
+++ toh-pt07restart/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroService} from "../service/hero.service";
+import {Hero} from "../bean/Hero";
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[];
+
+  constructor(
+    private heroService : HeroService,
+
+  ) { }
 
   ngOnInit() {
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+
+
+
+
 
 }
